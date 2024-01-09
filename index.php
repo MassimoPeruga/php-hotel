d6f74d3e20d634a75bbb55c9ea730061a8a6df76
MassimoPeruga <spamma-mutevole0p@icloud.com>
2024-01-09 17:09:26 +0100

    feat: Milestone 1

--- index.php
+++ index.php
@@ -1,3 +1,42 @@
+<?php
+$hotels = [
+    [
+        'name' => 'Hotel Belvedere',
+        'description' => 'Hotel Belvedere Descrizione',
+        'parking' => true,
+        'vote' => 4,
+        'distance_to_center' => 10.4
+    ],
+    [
+        'name' => 'Hotel Futuro',
+        'description' => 'Hotel Futuro Descrizione',
+        'parking' => true,
+        'vote' => 2,
+        'distance_to_center' => 2
+    ],
+    [
+        'name' => 'Hotel Rivamare',
+        'description' => 'Hotel Rivamare Descrizione',
+        'parking' => false,
+        'vote' => 1,
+        'distance_to_center' => 1
+    ],
+    [
+        'name' => 'Hotel Bellavista',
+        'description' => 'Hotel Bellavista Descrizione',
+        'parking' => false,
+        'vote' => 5,
+        'distance_to_center' => 5.5
+    ],
+    [
+        'name' => 'Hotel Milano',
+        'description' => 'Hotel Milano Descrizione',
+        'parking' => true,
+        'vote' => 2,
+        'distance_to_center' => 50
+    ],
+];
+?>
 <!DOCTYPE html>
 <html lang="en">
 
@@ -8,6 +47,27 @@
 </head>
 
 <body>
+
+    <main>
+        <ol>
+            <?php
+            // Ciclo esterno per attraversare gli hotel
+            foreach ($hotels as $hotel) {
+                // Inizia la lista non ordinata per ogni hotel
+                echo '<li><ul>';
+
+                // Ciclo interno per attraversare le informazioni di ciascun hotel
+                foreach ($hotel as $key => $value) {
+                    // Aggiunge ogni informazione come elemento di lista
+                    echo '<li>' . ucfirst(str_replace('_', ' ', $key)) . ': ' . ($key === 'parking' ? ($value ? 'Yes' : 'No') : $value) . '</li>';
+                }
+
+                // Chiude la lista non ordinata per ogni hotel
+                echo '</ul><hr></li>';
+            }
+            ?>
+        </ol>
+    </main>
 
 </body>
 
